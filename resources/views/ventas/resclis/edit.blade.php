e1b6c5634e14f6b945a471b3c54b6006a29de694
David Utreras <dutreras@azurian.com>
2025-03-28 13:19:10 -0300

    update detail views /ventas/solicitudes/edit.blade.php repair rooms selected

--- resources/views/ventas/resclis/edit.blade.php
+++ resources/views/ventas/resclis/edit.blade.php
@@ -511,7 +511,7 @@
                                         <div class="tab-pane fade" id="tourhoteles" role="tabpanel">
                                         @php
                                             
-                                            $habitacionesSeleccionadas = collect($rescli->habitaciones ?? []);
+                                            $habitacionesSeleccionadas = collect($rescli->habitaciones);
 
                                          @endphp
                                             @foreach($hotelesSeleccionados as $key => $hotelIds)
@@ -532,16 +532,15 @@
                                                                     @foreach($habitaciones->where('hotel_id', $hotel->id) as $habitacion)
                                                                         <div class="form-check form_habi{{ $habitacion->id }}{{ $key }}">
                                                                             <!-- ID único para los checkbox buttons y name basado en el día para selección única -->
-                                                                            <input class="form-check-input habitacion-checkbox" type="radio"
-                                                                                value="{{ $habitacion->id }}"
+                                                                            <input class="form-check-input habitacion-checkbox" type="checkbox" value="{{ $habitacion->id }}"
                                                                                 id="form_habi_{{ $hotel->id }}_{{ $habitacion->id }}_dia{{ $key }}"
                                                                                 name="habitacion_dia_{{ $key }}"
                                                                                 data-name="{{ $habitacion->titulo }}"
                                                                                 data-hnac="{{ number_format($habitacion->nacionales, 2, '.', '') }}"
                                                                                 data-hext="{{ number_format($habitacion->extranjeros, 2, '.', '') }}"
                                                                                 data-tit="{{ $hotel->titulo }}"
-                                                                                @if($habitacion->id == $habitSeleccionada) checked @endif
-                                                                            />
+                                                                                @if($rescli->habitaciones) @foreach($habitaciones as $habit) @if($habit["id"] == $habitacion->id) checked @endif @endforeach @endif />
+
                                                                             <label class="form-check-label" for="form_habi_{{ $hotel->id }}_{{ $habitacion->id }}_dia{{ $key }}">
                                                                                 {{ $habitacion->titulo }}
                                                                                 <span class="seccion-mexico hidden">Bs. {{ number_format($habitacion->nacionales, 2, '.', '') }}</span>
