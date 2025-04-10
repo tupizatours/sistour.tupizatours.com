7c4afa4af5ec920e7a3562725fc9439af0b2efd0
David Utreras <dutreras@azurian.com>
2025-04-10 12:08:10 -0400

    refactor anticipo y pago de servicios

--- resources/views/despachos/gestiones/show.blade.php
+++ resources/views/despachos/gestiones/show.blade.php
@@ -620,6 +620,22 @@
 
             <div class="col-lg-6">
               
+                @if(gestion)
+                <x-caja.operacion-prestatario 
+                    :reserva="$reserva" 
+                    :gestion="$gestion" 
+                    :propietarios="$prestatariosEnGestion" 
+                />
+                
+                <x-caja.operacion-totalidad 
+                    :totalGeneralHoteles="$totalGeneralHoteles"
+                    :totalGeneralTickets="$totalGeneralTickets"
+                    :totalGeneralAccesorios="$totalGeneralAccesorios"
+                    :totalGeneralServicios="$totalGeneralServicios"
+                    :totalGeneralGasto="$totalGeneralGasto"
+                    :prestatarios="$propietarios"
+                />
+            @endif
             </div>
         </div>
     </div>
